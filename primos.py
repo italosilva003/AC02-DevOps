bbbb3b258d2b33f17391ab2176324f1ee420ea38
italosilva003 <60719198+italosilva003@users.noreply.github.com>
2020-04-02 20:13:36 -0300

    ac02

---
+++ primos.py
@@ -0,0 +1,27 @@
+@app.route('/')
+def nao_entre_em_panico():
+    primos = []
+    for div in range(1,550+1):
+        cont = 0
+
+        for x in range(1,div+1):
+            if div % x == 0:
+                cont +=1
+
+        if cont == 2:
+            primos.append(x)
+
+    primos1 = ''
+    a = 0
+    b = 10
+    for j in range(1,10+1):
+        for i in range(a,b):
+            primos1 += str(primos[i]) + ',' 
+        primos1 += ' --> ' + str(b) + '<br>'
+        a += 10
+        b += 10
+    return primos1
+
+if name == "main":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
